3f1b126aa4b1f6fa9341f72723ae271a1c361c2b
Bill1235813 <billjohn1235813@gmail.com>
2018-04-10 16:58:56 -0700

    :art: make better figure

--- check_save_transitions.py
+++ check_save_transitions.py
@@ -13,44 +13,75 @@ from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 ZERO_COLOR = [0.67, 1, 1]
 FULL_COLOR = [0.7, 0.7, 0.7]
 PIXEL_COLOR = [1, 0, 0]
-Q_PATH = './save_q/'
+Q_PATH = './save_q/fig-star/8/'
 # temp = pickle.load(open('./save_all_transitions/transition_num001/game.p', mode='rb'))
-# temp = pickle.load(open(Q_PATH + 'fig.p', mode='rb'))
-temp = pickle.load(open(Q_PATH + 'influence.p', mode='rb'))
+temp = pickle.load(open(Q_PATH + 'fig.p', mode='rb'))
+observation = pickle.load(open('./save_q/influence.p', mode='rb'))
 
-temp = [(0 if i < 0 else i) for i in temp]
+temps = [(0 if i < 0 else i) for i in observation]
 observation = []
 for i in range(4):
-  observation.append(np.reshape(temp[i * 3600:(i + 1) * 3600], (45, 80)))
+  observation.append(np.reshape(temps[i * 3600:(i + 1) * 3600], (45, 80)))
+#
 
 # print(observation[0])
-first_image_plot = np.zeros((81, 81))
+# first_image_plot = np.zeros((81, 81))
+first_image_plot = np.zeros((80, 80, 3))
+medium = np.zeros((45, 80))
 for k in range(4):
-  first_image = np.zeros((81, 45))
-  first_image[:80] = np.transpose(np.flip(observation[k], axis=1))
-  first_image_plot[:][10:55] += np.transpose(first_image)
-  # x = []
-  # y = []
-  # for i in range(80):
-  #   for j in range(80):
-  #     if first_image[i][j] == 0:
-  #       first_image_plot[i][j] = ZERO_COLOR
-  #     elif first_image[i][j] == 255:
-  #       first_image_plot[i][j] = FULL_COLOR
-  #     else:
-  #       x.append(j)
-  #       y.append(i)
-  #       first_image_plot[i][j] = PIXEL_COLOR
-for i in range(50, 75):
-  for j in range(80):
-    if random.randint(0, 1000) % 100 == 0:
-      first_image_plot[i][j] += random.random() / 100
-for i in range(80):
-  for j in range(80):
-    first_image_plot[i][j] *= 2.5
-sns.set(font_scale=2)
-fig = sns.heatmap(first_image_plot, xticklabels=20, yticklabels=20,
-                  vmin = 0, vmax = 0.05, cmap='Blues')
+  medium += 2.5 * np.flip(observation[k], axis=1)
+print(sorted(np.reshape(medium, 3600)))
+for k in range(4):
+  # first_image = np.zeros((81, 45))
+  first_image = np.flip(temp[k], axis=1)
+  # medium += 2.5*np.flip(observation[k], axis=1)
+  # first_image[:80] = np.transpose(np.flip(observation[k], axis=1))
+  # first_image_plot[:][10:55] += np.transpose(first_image)
+  x = []
+  y = []
+  for i in range(80):
+    for j in range(80):
+      if first_image[i][j] == 0:
+        first_image_plot[i][j] = ZERO_COLOR
+      elif first_image[i][j] == 255:
+        first_image_plot[i][j] = FULL_COLOR
+      elif j <= 7:
+        first_image_plot[i][j] = ZERO_COLOR if first_image[i][j] == 40 else FULL_COLOR
+      else:
+        x.append(j)
+        y.append(i)
+        first_image_plot[i][j] = PIXEL_COLOR
+# medium = np.reshape(medium, 3600)
+# medium_zero = sum([(True if i != 0 else False) for i in medium])
+# medium_value = sorted(medium)[3600-int(medium_zero/2)]
+# mean_value = sum(medium)/medium_zero
+
+# print mean
+# print(medium_zero)
+# print(sorted(medium))
+# print(medium_value)
+# print(mean_value)
+# print(sum(medium)/3600)
+
+# # add random
+# for i in range(50, 75):
+#   for j in range(80):
+#     if random.randint(0, 1000) % 100 == 0:
+#       first_image_plot[i][j] += random.random() / 100
+# # enlarge
+# for i in range(80):
+#   for j in range(80):
+#     first_image_plot[i][j] *= 2.5
+# remove below mean
+# for i in range(80):
+#   for j in range(80):
+#     if (first_image_plot[i][j] < mean_value):
+#       first_image_plot[i][j] = 0
+
+# plot
+# sns.set(font_scale=2.5)
+# fig = sns.heatmap(first_image_plot, xticklabels=20, yticklabels=20,
+#                   vmin = 0, vmax = 0.05, cmap='Blues')
 # fig.axis([0, 81, 81, 0])
 # major_ticks = np.arange(0, 81, 20)
 # print(major_ticks)
@@ -74,12 +105,12 @@ fig = sns.heatmap(first_image_plot, xticklabels=20, yticklabels=20,
 # ax.xaxis.set_minor_locator(ticker.MultipleLocator(10))
 # fig.xaxis.set_ticks(np.arange(0, 81, 20))
 # fig.yaxis.set_ticks(np.arange(0, 81, 20))
-# fig = plt.imshow(first_image_plot)
-# plt.plot(x, y, 'r*', markersize = 10)
-# fig.axes.get_xaxis().set_visible(False)
-# fig.axes.get_yaxis().set_visible(False)
-plt.savefig(Q_PATH + 'another_blue_inf')
-plt.show()
+  fig = plt.imshow(first_image_plot)
+  plt.plot(x, y, 'r*', markersize = 10)
+  fig.axes.get_xaxis().set_visible(False)
+  fig.axes.get_yaxis().set_visible(False)
+  plt.savefig(Q_PATH + 'fig_star_clean' + str(k))
+  plt.show()
 
 # plt.imshow(second_image)
 # plt.savefig('save_q/fig2')
